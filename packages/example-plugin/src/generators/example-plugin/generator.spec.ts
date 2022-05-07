07218af205b652a1de269938a7696099b6fe0cbe
Goran Mržljak <goran.mrzljak@gmail.com>
2022-05-07 09:36:48 +0200

    initial commit

---
+++ packages/example-plugin/src/generators/example-plugin/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { ExamplePluginGeneratorSchema } from './schema';
+
+describe('example-plugin generator', () => {
+  let appTree: Tree;
+  const options: ExamplePluginGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/example-plugin/src/generators/example-plugin/schema.d.ts
@@ -0,0 +1,5 @@
+export interface ExamplePluginGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
