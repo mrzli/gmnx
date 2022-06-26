cfa8cfeac7c3023bc0d9f1a7a036e99895b267be
Goran Mržljak <goran.mrzljak@gmail.com>
2022-06-26 13:38:17 +0200

    refactor generator

--- packages/util/src/generators/util/generator.ts
+++ packages/util/src/generators/util/generator.ts
@@ -7,21 +7,11 @@ import {
   // offsetFromRoot,
   Tree,
 } from '@nrwl/devkit';
-// import * as path from 'path';
 import { UtilGeneratorSchema } from './schema';
-import { ClocExecutorSchema } from '../../executors/cloc/schema';
-import { PublishAllExecutorSchema } from '../../executors/publish-all/schema';
-import { MongoStartExecutorSchema } from '../../executors/mongo-start/schema';
-import { MongoStopExecutorSchema } from '../../executors/mongo-stop/schema';
-import { PostgresStartExecutorSchema } from '../../executors/postgres-start/schema';
-import { PostgresStopExecutorSchema } from '../../executors/postgres-stop/schema';
-
-interface NormalizedSchema extends UtilGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
-}
+import {
+  getProjectConfiguration,
+  NormalizedSchema,
+} from './src/project-configuration';
 
 export default async function (
   tree: Tree,
@@ -29,92 +19,13 @@ export default async function (
 ): Promise<void> {
   const normalizedOptions = normalizeOptions(tree, options);
 
-  const clocOptions: ClocExecutorSchema = {
-    ignoreDirs: ['.idea', '.vscode', 'node_modules', 'dist'],
-    ignoreFiles: ['package-lock.json'],
-  };
+  const projectConfiguration = getProjectConfiguration(normalizedOptions);
 
-  const mongoOptions: MongoStartExecutorSchema & MongoStopExecutorSchema = {
-    containerName: 'mongo',
-    mongoVersion: '5.0.8',
-    port: 27017,
-    dataDir: '~/docker/mongo',
-  };
-
-  const mongoStartOptions: MongoStartExecutorSchema = {
-    containerName: mongoOptions.containerName,
-    mongoVersion: mongoOptions.mongoVersion,
-    port: mongoOptions.port,
-    dataDir: mongoOptions.dataDir,
-  };
-  const mongoStopOptions: MongoStopExecutorSchema = {
-    containerName: mongoOptions.containerName,
-    mongoVersion: mongoOptions.mongoVersion,
-    port: mongoOptions.port,
-    dataDir: mongoOptions.dataDir,
-  };
-
-  const postgresOptions: PostgresStartExecutorSchema &
-    PostgresStopExecutorSchema = {
-    containerName: 'postgres',
-    postgresVersion: '14.2',
-    port: 15432,
-    dataDir: '~/docker/postgres',
-    username: 'postgres',
-    password: 'password',
-  };
-
-  const postgresStartOptions: PostgresStartExecutorSchema = {
-    containerName: postgresOptions.containerName,
-    postgresVersion: postgresOptions.postgresVersion,
-    port: postgresOptions.port,
-    dataDir: postgresOptions.dataDir,
-    username: postgresOptions.username,
-    password: postgresOptions.password,
-  };
-  const postgresStopOptions: PostgresStopExecutorSchema = {
-    containerName: postgresOptions.containerName,
-    postgresVersion: postgresOptions.postgresVersion,
-    port: postgresOptions.port,
-    dataDir: postgresOptions.dataDir,
-    username: postgresOptions.username,
-    password: postgresOptions.password,
-  };
-
-  const publishAllOptions: PublishAllExecutorSchema = {};
-
-  addProjectConfiguration(tree, normalizedOptions.projectName, {
-    root: normalizedOptions.projectRoot,
-    projectType: 'library',
-    sourceRoot: `${normalizedOptions.projectRoot}/src`,
-    targets: {
-      cloc: {
-        executor: '@gmnx/util:cloc',
-        options: clocOptions,
-      },
-      'mongo-start': {
-        executor: '@gmnx/util:mongo-start',
-        options: mongoStartOptions,
-      },
-      'mongo-stop': {
-        executor: '@gmnx/util:mongo-stop',
-        options: mongoStopOptions,
-      },
-      'postgres-start': {
-        executor: '@gmnx/util:postgres-start',
-        options: postgresStartOptions,
-      },
-      'postgres-stop': {
-        executor: '@gmnx/util:postgres-stop',
-        options: postgresStopOptions,
-      },
-      'publish-all': {
-        executor: '@gmnx/util:publish-all',
-        options: publishAllOptions,
-      },
-    },
-    tags: normalizedOptions.parsedTags,
-  });
+  addProjectConfiguration(
+    tree,
+    normalizedOptions.projectName,
+    projectConfiguration
+  );
   // addFiles(tree, normalizedOptions);
   await formatFiles(tree);
 }
---
+++ packages/util/src/generators/util/src/project-configuration.ts
@@ -0,0 +1,106 @@
+import { ProjectConfiguration } from '@nrwl/devkit';
+import { ClocExecutorSchema } from '../../../executors/cloc/schema';
+import { MongoStartExecutorSchema } from '../../../executors/mongo-start/schema';
+import { MongoStopExecutorSchema } from '../../../executors/mongo-stop/schema';
+import { PostgresStartExecutorSchema } from '../../../executors/postgres-start/schema';
+import { PostgresStopExecutorSchema } from '../../../executors/postgres-stop/schema';
+import { PublishAllExecutorSchema } from '../../../executors/publish-all/schema';
+import { UtilGeneratorSchema } from '../schema';
+
+export interface NormalizedSchema extends UtilGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  parsedTags: string[];
+}
+
+export function getProjectConfiguration(
+  normalizedOptions: NormalizedSchema
+): ProjectConfiguration {
+  const clocOptions: ClocExecutorSchema = {
+    ignoreDirs: ['.idea', '.vscode', 'node_modules', 'dist'],
+    ignoreFiles: ['package-lock.json'],
+  };
+
+  const mongoOptions: MongoStartExecutorSchema & MongoStopExecutorSchema = {
+    containerName: 'mongo',
+    mongoVersion: '5.0.8',
+    port: 27017,
+    dataDir: '~/docker/mongo',
+  };
+
+  const mongoStartOptions: MongoStartExecutorSchema = {
+    containerName: mongoOptions.containerName,
+    mongoVersion: mongoOptions.mongoVersion,
+    port: mongoOptions.port,
+    dataDir: mongoOptions.dataDir,
+  };
+  const mongoStopOptions: MongoStopExecutorSchema = {
+    containerName: mongoOptions.containerName,
+    mongoVersion: mongoOptions.mongoVersion,
+    port: mongoOptions.port,
+    dataDir: mongoOptions.dataDir,
+  };
+
+  const postgresOptions: PostgresStartExecutorSchema &
+    PostgresStopExecutorSchema = {
+    containerName: 'postgres',
+    postgresVersion: '14.2',
+    port: 15432,
+    dataDir: '~/docker/postgres',
+    username: 'postgres',
+    password: 'password',
+  };
+
+  const postgresStartOptions: PostgresStartExecutorSchema = {
+    containerName: postgresOptions.containerName,
+    postgresVersion: postgresOptions.postgresVersion,
+    port: postgresOptions.port,
+    dataDir: postgresOptions.dataDir,
+    username: postgresOptions.username,
+    password: postgresOptions.password,
+  };
+  const postgresStopOptions: PostgresStopExecutorSchema = {
+    containerName: postgresOptions.containerName,
+    postgresVersion: postgresOptions.postgresVersion,
+    port: postgresOptions.port,
+    dataDir: postgresOptions.dataDir,
+    username: postgresOptions.username,
+    password: postgresOptions.password,
+  };
+
+  const publishAllOptions: PublishAllExecutorSchema = {};
+
+  return {
+    root: normalizedOptions.projectRoot,
+    projectType: 'library',
+    sourceRoot: `${normalizedOptions.projectRoot}/src`,
+    targets: {
+      cloc: {
+        executor: '@gmnx/util:cloc',
+        options: clocOptions,
+      },
+      'mongo-start': {
+        executor: '@gmnx/util:mongo-start',
+        options: mongoStartOptions,
+      },
+      'mongo-stop': {
+        executor: '@gmnx/util:mongo-stop',
+        options: mongoStopOptions,
+      },
+      'postgres-start': {
+        executor: '@gmnx/util:postgres-start',
+        options: postgresStartOptions,
+      },
+      'postgres-stop': {
+        executor: '@gmnx/util:postgres-stop',
+        options: postgresStopOptions,
+      },
+      'publish-all': {
+        executor: '@gmnx/util:publish-all',
+        options: publishAllOptions,
+      },
+    },
+    tags: normalizedOptions.parsedTags,
+  };
+}
