913cf73c07bc15b3dac4f5c9c74a7f127e7b4ec5
Goran Mržljak <goran.mrzljak@gmail.com>
2022-06-29 22:23:53 +0200

    implement project:data-model generator

--- packages/project/src/generators/data-model/generator.ts
+++ packages/project/src/generators/data-model/generator.ts
@@ -5,6 +5,7 @@ import {
   getWorkspaceLayout,
   names,
   offsetFromRoot,
+  ProjectConfiguration,
   Tree,
 } from '@nrwl/devkit';
 import * as path from 'path';
@@ -14,10 +15,35 @@ interface NormalizedSchema extends DataModelGeneratorSchema {
   projectName: string;
   projectRoot: string;
   projectDirectory: string;
-  parsedTags: string[]
+  parsedTags: string[];
 }
 
-function normalizeOptions(tree: Tree, options: DataModelGeneratorSchema): NormalizedSchema {
+export default async function (
+  tree: Tree,
+  options: DataModelGeneratorSchema
+): Promise<void> {
+  const normalizedOptions = normalizeOptions(tree, options);
+
+  const projectConfiguration: ProjectConfiguration = {
+    root: normalizedOptions.projectRoot,
+    projectType: 'library',
+    sourceRoot: `${normalizedOptions.projectRoot}/src`,
+    tags: normalizedOptions.parsedTags,
+  };
+
+  addProjectConfiguration(
+    tree,
+    normalizedOptions.projectName,
+    projectConfiguration
+  );
+  addFiles(tree, normalizedOptions);
+  await formatFiles(tree);
+}
+
+function normalizeOptions(
+  tree: Tree,
+  options: DataModelGeneratorSchema
+): NormalizedSchema {
   const name = names(options.name).fileName;
   const projectDirectory = options.directory
     ? `${names(options.directory).fileName}/${name}`
@@ -37,33 +63,17 @@ function normalizeOptions(tree: Tree, options: DataModelGeneratorSchema): Normal
   };
 }
 
-function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...names(options.name),
-      offsetFromRoot: offsetFromRoot(options.projectRoot),
-      template: ''
-    };
-    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
-}
-
-export default async function (tree: Tree, options: DataModelGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(
+function addFiles(tree: Tree, options: NormalizedSchema): void {
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: '',
+  };
+  generateFiles(
     tree,
-    normalizedOptions.projectName,
-    {
-      root: normalizedOptions.projectRoot,
-      projectType: 'library',
-      sourceRoot: `${normalizedOptions.projectRoot}/src`,
-      targets: {
-        build: {
-          executor: "@gmnx/project:build",
-        },
-      },
-      tags: normalizedOptions.parsedTags,
-    }
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
   );
-  addFiles(tree, normalizedOptions);
-  await formatFiles(tree);
 }
