0cff624ff851b22582ac44e69e4ab3d40259a6df
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-02 10:28:14 +0200

    add stub code generators

---
+++ packages/project/src/generators/backend-app-code/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { BackendAppCodeGeneratorSchema } from './schema';
+
+describe('backend-app-code generator', () => {
+  let appTree: Tree;
+  const options: BackendAppCodeGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  })
+});
---
+++ packages/project/src/generators/shared-library-code/schema.d.ts
@@ -0,0 +1,5 @@
+export interface SharedLibraryCodeGeneratorSchema {
+    name: string;
+    tags?: string;
+    directory?: string;
+}
