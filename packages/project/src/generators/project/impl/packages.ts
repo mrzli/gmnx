4d2d00b8792c42d39abf3bc71f68e326e06f2934
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-07 22:42:39 +0200

    small updates + publish

--- packages/project/src/generators/project/impl/packages.ts
+++ packages/project/src/generators/project/impl/packages.ts
@@ -2,12 +2,16 @@ import { addDependenciesToPackageJson, Tree } from '@nrwl/devkit';
 
 const VERSION_MONGODB = '4.7.0';
 
+const VERSION_TYPE_FEST = '2.14.0';
+
 export async function installPackages(tree: Tree): Promise<void> {
   await addDependenciesToPackageJson(
     tree,
     {
       mongodb: VERSION_MONGODB,
     },
-    {}
+    {
+      'type-fest': VERSION_TYPE_FEST,
+    }
   );
 }
