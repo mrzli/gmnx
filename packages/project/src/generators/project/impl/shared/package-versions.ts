26f6c832cf47453a8ea4b12bdaa297a1c80d4cf4
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-16 21:57:04 +0200

    update project generator package versions

--- packages/project/src/generators/project/impl/shared/package-versions.ts
+++ packages/project/src/generators/project/impl/shared/package-versions.ts
@@ -1,8 +1,8 @@
 export const PROJECT_PACKAGES = {
   autoprefixer: '10.4.7',
   axios: '0.27.2',
-  mongodb: '4.7.0',
+  mongodb: '4.8.0',
   postcss: '8.4.14',
-  tailwindcss: '3.1.5',
-  'type-fest': '2.14.0',
+  tailwindcss: '3.1.6',
+  'type-fest': '2.16.0',
 } as const;
